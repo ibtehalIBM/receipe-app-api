6410dcc8b5f490121a5be719b659cfbe2457c919
Ibtehal.Moahmed <Ibtehal.Moahmed@syngenta.com>
2023-06-15 16:11:33 -0400

    updates

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -131,5 +132,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS' : 'drf-spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS' : 'drf_spectacular.openapi.AutoSchema',
 }
--- app/app/urls.py
+++ app/app/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from drf-spectacular.views import {
+from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-}
+)
+from django.contrib import admin
+from django.urls import path,include
+from  . import user
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
@@ -26,5 +29,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user', include('user.urls')),
 ]
